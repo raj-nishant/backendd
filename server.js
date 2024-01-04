316f7aa6c9775567fc5014c3c5c97636e4e8812e
Nishant Raj <79581018+raj-nishant@users.noreply.github.com>
2024-01-04 16:12:36 +0530

    u

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const cors = require("cors");
 const app = express();
 const PORT = 5000;
 
-// Enable CORS for all routes
 app.use(cors());
 
-// Connect to MongoDB using the connection string from the environment variables
 mongoose.connect(
   "mongodb+srv://xenon:xenon@cluster0.x9fpvih.mongodb.net/?retryWrites=true&w=majority",
   {
@@ -17,10 +15,8 @@ mongoose.connect(
   }
 );
 
-// Middleware
 app.use(express.json());
 
-// MongoDB schema
 const contactSchema = new mongoose.Schema({
   name: String,
   email: String,
@@ -42,50 +38,51 @@ const UserSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", UserSchema);
 
-// Route for handling contact form submissions
 app.post("/api/contact", async (req, res) => {
   try {
     const { name, email, message } = req.body;
-
-    // Create a new contact document
     const newContact = new ContactModel({ name, email, message });
-
-    // Save the document to MongoDB
     await newContact.save();
-
     res
       .status(200)
       .json({ success: true, message: "Contact form submitted successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({
-      success: false,
-      message: "Internal server error",
-      error: error.message,
-    });
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Internal server error",
+        error: error.message,
+      });
   }
 });
 
 app.post("/register", async (req, res) => {
   try {
     const { username, password } = req.body;
-    console.log(req.body);
     const user = new User({ username, password });
     await user.save();
     res.status(201).json({ message: "Registration Successful" });
   } catch (error) {
-    res.status(500).json({ error: "Registration failed" });
+    console.error(error);
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Internal server error",
+        error: error.message,
+      });
   }
 });
 
-// Login
 app.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
 
     if (!user) {
-      return res.status(401).json({ error: "Invalid username or Password" });
+      return res.status(401).json({ error: "Invalid username or password" });
     }
 
     if (user.password !== password) {
@@ -93,11 +90,17 @@ app.post("/login", async (req, res) => {
     }
     res.status(200).json({ message: "Login successful" });
   } catch (error) {
-    res.status(500).json({ error: "Login failed" });
+    console.error(error);
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Internal server error",
+        error: error.message,
+      });
   }
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
